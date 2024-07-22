c554fff8b06aded2a0002cc90b16eb70c4228e74
Angelo Magno <76539988+Angelo-Magno@users.noreply.github.com>
2024-07-22 20:00:16 -0300

    Update: initiated oop implementation

--- README.md
+++ README.md
@@ -4,20 +4,20 @@
 
 ### **``Sobre:``**
 
-O projeto é focado em construir um sistema bancario, partindo de três desafios propostos: 
+O projeto é focado em construir um sistema bancário, partindo de três desafios propostos: 
 
-1. O primeiro deles proponhe uma abordagem mais simples utilizando apenas estruturas de controle basicas:
+1. O primeiro deles propõe uma abordagem mais simples utilizando apenas estruturas de controle basicas:
 
-    **Desafio:** [Criando um Sistema Bancário com Python](pp/pp_bank_system.py)
+    **Desafio:** [Criando um Sistema Bancário com Python](pp)
 
-2. O segundo proponhe uma abordagem mais modular utilizando funções:
+2. O segundo propõe uma abordagem mais modular utilizando funções:
 
-    **Desafio:** [Otimizando o Sistema Bancário com Funções Python](fp/fp_bank_system.py)
+    **Desafio:** [Otimizando o Sistema Bancário com Funções Python](fp)
 
 3. Por fim é utilizado o paradigma orientado a objetos:
 
-    **Desafio:** [Modelando o Sistema Bancário em POO com Python](oop/oop_bank_system.py)
+    **Desafio:** [Modelando o Sistema Bancário em POO com Python](oop)
 
 Agora para complementar o conhecimento vamos além dos desafios propostos, implementando novas funcionalidades e otimizando o código:
 
-**Desafio pessoal:** [Indo Além do Basico]()
+**Desafio pessoal:** [Indo Além do Básico]()
--- oop/oop_bank_system.py
+++ oop/oop_bank_system.py
@@ -1,249 +1,149 @@
 import textwrap
-import os
+from abc import ABC, abstractmethod
 
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
-        self.contas = {}
+        self.contas = []
 
 
 class PessoaFisica(Cliente):
     def __init__(self, cpf, nome, data_nascimento, endereco):
-        super().__init__(endereco)
+        super().__init__(endereco=endereco)
         self.cpf = cpf
         self.nome = nome
         self.data_nascimento = data_nascimento
-    
+
+
+class PessoaJuridica(Cliente):
+    def __init__(self, cnpj, razao_social, endereco):
+        super().__init__(endereco=endereco)
+        self.cnpj = cnpj
+        self.razao_social = razao_social
+
 
 class Conta:
-    pass
+    def __init__(self, numero):
+        self._numero = numero
+        self._agencia = '0382'
+        self._saldo = 0
+
 
 class ContaCorrente(Conta):
-    pass
+    def __init__(self, numero):
+        super().__init__(numero=numero)
+
+
+class ContaPoupanca(Conta):
+    def __init__(self, numero):
+        super().__init__(numero=numero)
+
 
 class Historico:
-    pass
+    def __init__(self):
+        self._transacoes = []
 
-class Transacao:
-    pass
 
-class Deposito(Transacao):
-    pass
+class Transacao(ABC):  # interface
+    @property
+    @abstractmethod
+    def valor(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+
 
 class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    def valor(self):
+        pass
+
+    def registrar(self, conta):
+        pass
+
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    def valor(self):
+        pass
+
+    def registrar(self, conta):
+        pass
+
+
+class Tranferencia(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    def valor(self):
+        pass
+
+    def registrar(self, conta):
+        pass
+
+
+def depositar(saldo, valor, extrato, /):  # Positional only
     pass
 
 
-
-cores = {'limpar': '\033[m',
-         'verde': '\033[32m',
-         'vermelho': '\033[31m',
-         }
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):  # keyword only
+    pass
 
 
-def menu():
-    menu = """
-    =============== MENU ===============
-        [1] Depositar
-        [2] Sacar
-        [3] Extrato
-        [4] Novo usuário
-        [5] Nova conta
-        [6] Listar usuários
-        [7] Listar contas
-        [0] Sair
-    > """
-    return int(input(textwrap.dedent(menu)))
+def exibir_extrato(saldo, /, *, extrato):  # positional only | keyword only
+    pass
 
 
-def espera():
-    while True:
-        op = input('\nPressione ENTER para voltar ao menu!').strip()
-        if op == '':
-            break
+def criar_usuario():
+    pass
 
 
-def depositar(saldo, valor, extrato, /):
-    if valor > 0:
-        saldo += valor
-        extrato += f"\t{cores['verde']}+ R$ {valor:.2f}{cores['limpar']}\n"
-        print(f"{cores['verde']}Operação realizada com sucesso!{cores['limpar']}\n"
-              f'Seu saldo atual é de R$: {saldo:.2f}')
-        espera()
-        return saldo, extrato
-    
-    print(f"{cores['vermelho']}Operação falhou! O valor informado é inválido.{cores['limpar']}")
-    espera()
-
-    return saldo, extrato
-
-
-def sacar(*, saldo, valor, extrato, limite_valor_saque, numero_saques, limite_saques):
-
-    if valor > 0:
-
-        if valor > limite_valor_saque:
-            print(f"{cores['vermelho']}Operação inválida! "
-                  f"O valor do saque excede o seu limite.{cores['limpar']}")
-            espera()
-
-        elif valor > saldo:
-            print(f"{cores['vermelho']}Operação inválida! Saldo insuficiente.{cores['limpar']}\n"
-                  f"Saldo atual R$: {saldo:.2f}")
-            espera()
-
-        elif numero_saques >= limite_saques:
-            print(f"{cores['vermelho']}Operação inválida! "
-                  f"Você atingiu o seu limite máximo de saques diário.{cores['limpar']}")
-            espera()
-
-        else:
-            saldo -= valor
-            extrato += f"\t{cores['vermelho']}- R$ {valor:.2f}{cores['limpar']}\n"
-            numero_saques += 1
-            print(f"{cores['verde']}Operação realizada com sucesso!{cores['limpar']}\n"
-                  f'Seu saldo atual é de R$: {saldo:.2f}')
-            espera()
-
-    else:
-        print(f"{cores['vermelho']}Valor inválido!{cores['limpar']}")
-        espera()
-        
-
-    return saldo, extrato
-
-
-def exibir_extrato(saldo, /, *, extrato):
-    print("\n" + " EXTRATO ".center(33, "="))
-    print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"Saldo Total: R$ {saldo:.2f}")
-    print("=" * 33)
-    espera()
-
-
-def criar_cliente(clientes):
-    cpf = input("Informe o CPF (somente números): ")
-
-    if clientes.get(cpf):
-        print(f"\n{cores['vermelho']}Usuário com CPF: {cpf} já possui cadastro!{cores['limpar']}")
-        espera()
-        return
-    
-    nome = input("Informe o nome completo: ")
-    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
-    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
-    clientes[cpf] = PessoaFisica(cpf=cpf, nome=nome, data_nascimento=data_nascimento, endereco=endereco)
-
-    print(f"\n{cores['verde']}Usuário criado com sucesso!{cores['limpar']}")
-    espera()
-
-
-def listar_usuarios(usuarios):
-    if len(usuarios) == 0:
-        print(f"\n{cores['vermelho']}Não há usuários cadastrados!{cores['limpar']}")
-        espera()
-        return
-    
-    for cpf, dados_usuario in usuarios.items():
-        print(f'Nome: {dados_usuario['nome']},  CPF: {cpf}')
-    espera()
-
-
-def criar_conta(agencia, contas, usuarios):
-    cpf = input("Informe o CPF do usuário: ")
-    try:
-        usuario = usuarios[cpf]
-        numero_conta = len(contas) + 1
-        contas[numero_conta] = {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
-        print(f"\n{cores['verde']}Conta criada com sucesso!{cores['limpar']}")
-        espera()
-
-    except KeyError:
-        print(f"\n{cores['vermelho']}Usuário não encontrado!\n"
-              "É preciso ser cadastrado para criar uma nova conta!\n"
-              f"Para criar uma novo usuário selecione a opcao 4!{cores['limpar']}")
-        espera()
-
-
-def listar_contas(contas):
-    if len(contas) == 0:
-        print(f"\n{cores['vermelho']}Não há contas cadastradas!{cores['limpar']}")
-        espera()
-        return
- 
-    for conta in contas.values():
-        linha = f"""\
-            Agência:\t{conta['agencia']}
-            C/C:\t\t{conta['numero_conta']}
-            Titular:\t{conta['usuario']['nome']}
-        """
-        print("=" * 50)
-        print(textwrap.dedent(linha))
-    espera()
-
+def criar_conta():
+    pass
 
 def main():
-    LIMITE_SAQUES = 3
-    AGENCIA = "0001"
+    MENU = '''
+        ========= MENU ==========
+            [1] Depositar
+            [2] Sacar
+            [3] Exibir Extrato
+            [4] Criar Usuário
+            [5] Criar Conta
+            [6] Listar Usúarios
+            [7] Listar Contas
+            [0] Sair
+        Digite uma opção: '''
 
-    saldo = 0
-    limite = 500
-    extrato = ""
-    numero_saques = 0
     clientes = {}
-    contas = {}
+    contas_contador = 0
 
     while True:
-        opcao = menu()
-        os.system('cls')
-
+        opcao = int(input(textwrap.dedent(MENU)))
         match opcao:
-
-            case 1:  # Depositar
-                valor = float(input("Informe o valor do depósito: "))
-                
-                saldo, extrato = depositar(saldo, valor, extrato)
-                os.system('cls')
-
-            case 2:  # Sacar
-                valor = float(input("Informe o valor do saque: "))
-            
-                saldo, extrato = sacar(
-                    saldo=saldo,
-                    valor=valor,
-                    extrato=extrato,
-                    limite_valor_saque=limite,
-                    numero_saques=numero_saques,
-                    limite_saques=LIMITE_SAQUES,
-                )
-                os.system('cls')
-
-            case 3:  # Extrato
-                exibir_extrato(saldo, extrato=extrato)
-                os.system('cls')
-
-            case 4:  # Usuario
-                criar_cliente(clientes)
-                os.system('cls')
-
-            case 5:  # Conta
-                criar_conta(AGENCIA, contas, clientes)
-                os.system('cls')
-            
-            case 6:  # Listar
-                listar_usuarios(clientes)
-                os.system('cls')
-
-            case 7:  # Listar 
-                listar_contas(contas)
-                os.system('cls')
-
-            case 0:  # Sair
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 0:
                 break
 
-            case _:
-                print(f"{cores['vermelho']}Opção inválida! Informe uma opção de 0 a 6!{cores['limpar']}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
